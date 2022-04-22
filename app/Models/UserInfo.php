e2f39724a21eb0ca1d2e502e01a16227691fd6c0
Joana06083 <joana06083@gmail.com>
2022-04-22 11:02:57 +0800

    2022042202-del

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -10,7 +10,4 @@ class UserInfo extends Model
     protected $primaryKey = 'userNo'; // 主鍵
     public $timestamps = false; // 沒有設定時間 created_at 或 updated_at 的欄位，不需要時間戳記
     public $incrementing = false; // 主鍵不是數字要設置
-    // protected $fillable =
-    //     ['userNo', 'userName', 'password', 'sex', 'email'];
-    // protected $guarded = ['password'];
 }
